e8c14656e2b4aaae16a70ebfc50cf63e9900c77a
Dodsie <baileydods@gmail.com>
2022-03-07 20:20:29 -0700

    add tail and lint head

--- tail.js
+++ tail.js
@@ -6,11 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(array) {
-  let value = array[0];
+const tail = function(array) {
+  let value = array.slice(1);
   return value;
-};
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([1]));
-console.log(head([]));
+}; console.log(tail([20,15,6,4,10,50,50,200,30,50,302]));
+
+const result = tail(["Bailey", "Taylor", "Coding", 700]);
+assertEqual(result.length, 3);
+assertEqual(result[0], "Taylor");
+assertEqual(result[2], 700);
