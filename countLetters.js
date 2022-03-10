556fc1275792ca6ef3dce2ed176b4ae784746b9c
Dodsie <baileydods@gmail.com>
2022-03-09 18:00:36 -0700

    eslinted

--- countLetters.js
+++ countLetters.js
@@ -11,16 +11,18 @@ const countLetters = function(string) {
   for (let letter of string) {
     if (numOfLetters[letter]) {
       numOfLetters[letter] += 1;
-    } else {numOfLetters[letter] = 1};
+    } else {
+      numOfLetters[letter] = 1;
+    }
   }
   return numOfLetters;
 };
 const result1 = countLetters("hello");
 console.log(result1);
-assertEqual(result1["h"],1)
-assertEqual(result1["e"],1)
-assertEqual(result1["l"],2)
-assertEqual(result1["o"],1)
+assertEqual(result1["h"],1);
+assertEqual(result1["e"],1);
+assertEqual(result1["l"],2);
+assertEqual(result1["o"],1);
 // initialize empty object
 // loop through string + initialize key or add for each iteration
 
