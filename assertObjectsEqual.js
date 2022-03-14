a3b4c95a53a1881fcda0a860f135b271e8489fad
Dodsie <baileydods@gmail.com>
2022-03-14 16:41:27 -0600

    fix

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,45 +1,11 @@
-const eqObjects = function(objectOne, objectTwo) {
-  if (Object.keys(objectOne).length !== Object.keys(objectTwo).length) {
-    return false;
-  }
-  for (let key in objectOne) {
-    if (!(objectOne[key] === Object)) {
-      if (objectOne[key] !== objectTwo[key]) return false;
-    } else if (objectOne[key] === Array) {
-      if (!eqArrays(objectOne[key],objectTwo[key])) return false;
-    } else {
-      if (!eqObjects(objectOne[key],objectTwo[key])) return false;
-    }
-  }
-  return true;
-};
-const eqArrays = function(arrayOne,arrayTwo) {
-  let lengthArrOne = arrayOne.length;
-  let lengthArrTwo = arrayTwo.length;
-  if (lengthArrOne !== lengthArrTwo) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  } return true;
-};
-
-
-const personDuplicate = {name: 'Bailey', lastName: 'Dods'};
-const person = {name: 'Bailey', lastName: 'Dods'};
-const person2 = {name: 'Tonka', lastName: 'Jahari'};
-const person2Duplicate = {name: 'Tonka', lastName: 'Jahari'};
-
-
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
   if (!eqObjects(actual,expected)) {
-    return `❌❌❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}!`;
+    return `❌❌❌Assertion Failed: ${actual} !== ${expected}!`;
   } else {
-    return `✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}!`;
+    return `✅✅✅Assertion Passed: ${actual} === ${expected}!`;
   }
 };
 
