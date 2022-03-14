3fdaa2139601e0f50a084a21fe0d7303f32d0f96
Dodsie <baileydods@gmail.com>
2022-03-14 14:12:33 -0600

    tailTest with chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Bailey", "Taylor", "Coding", 700]);
-assertEqual(result.length, 3);
-assertEqual(result[0], "Taylor");
-assertEqual(result[2], 700);
-assertEqual(result.length, 4);
-assertEqual(result[1], "Taylor");
-assertEqual(result[3], 700);
+describe("tail", () => {
+  it("should return array of every index but the first", () => {
+    assert.deepEqual(tail([1,2,3]),[2,3]);
+  });
+  it("should return array of every index but the first", () => {
+    assert.deepEqual(tail(['a','b','c']),['b','c']);
+  });
+  it("should return array of every index but the first", () => {
+    assert.deepEqual(tail(['a','b','c']),['a','b']); // should fail.
+  });
+});
